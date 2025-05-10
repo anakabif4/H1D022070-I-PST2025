6483a35638bdab08748986743fc35ea2108a1ee2
anakabif4 <151807926+anakabif4@users.noreply.github.com>
2025-05-10 22:02:28 +0700

    pert 5

---
+++ Pertemuan 2/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+
+// Variabel
+int echoPin = D1;
+int trigPin = D6;
+int ledPin = D3;
+
+// 
+long duration;
+int distance;
+
+void setup() {
+pinMode(echoPin, INPUT);
+pinMode(trigPin, OUTPUT);
+pinMode(ledPin, OUTPUT);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(6);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(11);
+  digitalWrite(trigPin,LOW);
+
+  //Menghitung waktu sinyal diterima
+  duration = pulseIn(echoPin,HIGH);
+
+  // Menghitung jarak
+  distance = duration*0.034/2;
+
+  //Display 
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print("cm");
+
+  if (distance<=10)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+  }
+  
+  delay(1000);
+}
---
+++ Pertemuan 5/src/main.cpp
@@ -0,0 +1,87 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "Imina";
+const char* password = "jiyongeomma";
+
+ESP8266WebServer server(80);
+String webpage;
+
+void setup() {
+Serial.begin(9600);
+delay(10);
+
+pinMode(D0, OUTPUT);
+pinMode(D2, OUTPUT);
+pinMode(D4, OUTPUT);
+
+// Connect to WiFi network
+Serial.println();
+Serial.print("Konfigurasi akses poin...");
+
+// Mengatur WiFi
+WiFi.mode(WIFI_AP);
+WiFi.begin(ssid, password);
+
+// Cek status koneksi
+Serial.println("IP address: ");
+Serial.println(WiFi.softAPIP());
+
+//Isi dari Webpage
+webpage+= "<h1> Web control ESP8266</h1>";
+webpage+= "<h2> Hasna</h2>";
+webpage+= "<p> LED 1 : ";
+webpage+= "<a href=\"LED1ON\"\"><button>ON</button></a><a href=\"LED1OFF\"\"><button>OFF</button></a></p><br>";
+webpage+= "<p>LED 2 : ";
+webpage+= "<a href=\"LED2ON\"\"><button>ON</button></a><a href=\"LED2OFF\"\"><button>OFF</button></a></p><br>";
+webpage+= "<p>LED 3 : ";
+webpage+= "<a href=\"LED3ON\"\"><button>ON</button></a><a href=\"LED3OFF\"\"><button>OFF</button></a></p><br>";
+
+// Membuat tampilan Webpage
+server.on("/", []() {
+  server.send(200, "text/html", webpage);
+});
+
+// Bagian ini untuk merespon perintah yang masuk
+server.on("/LED1ON", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D0,HIGH);
+  delay(1000);
+});
+server.on("/LED2ON", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D2,HIGH);
+  delay(1000);
+});
+server.on("/LED3ON", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D4,HIGH);
+  delay(1000);
+});
+server.on("/LED1OFF", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D0,LOW);
+  delay(1000);
+});
+server.on("/LED2OFF", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D2,LOW);
+  delay(1000);
+});
+server.on("/LED3OFF", [](){
+  server.send(200, "text/html", webpage);
+  digitalWrite(D4,LOW);
+  delay(1000);
+});
+
+server.begin();
+Serial.println("Webserver dijalankan");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+
